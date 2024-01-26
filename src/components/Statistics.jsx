6d45b28bc01b39f1adbfe2bbd642cacc02ed7654
AlejaEJ <alejaespejo96@gmail.com>
2024-01-25 20:04:47 -0500

    Exercise 1 and 2 with css

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,3 +1,6 @@
+import PropTypes from "prop-types";
+import css from "./Statistics.module.css";
+
 const Statistics = ({
     title,
     stats,
@@ -7,19 +10,30 @@ const Statistics = ({
       };
       
     return (
-        <section className="statistics">
-            {title && <h2 className="title">{title}</h2>}
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
             
-            <ul className="stat-list">
-                {stats.map(stat => (
-                    <li key={stat.id} className="item" style={{ backgroundColor: generateRandomColor() }}>
-                        <span className="label">{stat.label}</span>
-                        <span className="percentage">{stat.percentage}%</span>
+            <ul className={css["stat-list"]}>
+                {stats.map((stat) => (
+                    <li key={stat.id} className={css.item} style={{ backgroundColor: generateRandomColor() }}>
+                        <span className={css.label}>{stat.label}</span>
+                        <span className={css.percentage}>{stat.percentage}%</span>
                     </li>
                 ))}
             </ul>
         </section>
     );
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        }),
+    ),
 };
 
 export default Statistics;
